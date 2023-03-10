b20a9896af9f6f1a64bf9cd3c8840d5d76873d92
John Torres <johnnyjamestorres@outlook.com>
2023-03-10 14:30:28 -0800

    Consolidate device_groups test

--- tests/helpers_test.py
+++ tests/helpers_test.py
@@ -1,37 +1,22 @@
 """Tests for the helpers.py file."""
 
 import os
-from typing import Any, Optional, Tuple
+from typing import Any, Tuple
 
 import pytest
 
 from palo_sidekick.helpers import Panorama
 
 
-@pytest.fixture(scope="session")
-def data_dir() -> str:
-    here = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(here, "data")
-
-
-@pytest.fixture(scope="session")
-def env_vars() -> Tuple[Optional[str], Optional[str]]:
-    hostname = os.getenv("PANORAMA_HOSTNAME")
-    api_key = os.getenv("PANORAMA_KEY")
-    return hostname, api_key
-
-
-def test_env_vars_exist(env_vars: Tuple[str, str]) -> None:
+def test_env_vars_are_set(env_vars: Tuple[str, str]) -> None:
+    """Testing environment variables are set."""
     assert None not in env_vars
 
 
-@pytest.fixture(scope="session")
-def panorama(env_vars: Tuple[str, str]) -> Panorama:
-    return Panorama(*env_vars)
-
-
-@pytest.fixture
-def mock_get_device_groups(monkeypatch: pytest.MonkeyPatch, data_dir: str) -> None:
+def test_device_groups(
+    monkeypatch: pytest.MonkeyPatch, panorama: Panorama, data_dir: str
+) -> None:
+    """Testing XML for Panorama.device_groups is parsed correctly."""
     xml_file = "show_devicegroups.xml"
     with open(os.path.join(data_dir, xml_file)) as f:
         xml = f.read()
@@ -40,8 +25,5 @@ def mock_get_device_groups(monkeypatch: pytest.MonkeyPatch, data_dir: str) -> No
         return xml
 
     monkeypatch.setattr(Panorama, "get_device_groups", mock_xml)
-
-
-def test_device_groups(mock_get_device_groups: str, panorama: Panorama) -> None:
     expected = ["DG-1", "DG-2", "DG-3", "DG-4", "DG-5"]
     assert expected == panorama.device_groups
