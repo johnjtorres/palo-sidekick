46c435defd585f22417f3ba9259ad2338b3cb9c5
John Torres <johnnyjamestorres@outlook.com>
2023-03-09 17:11:06 -0800

    Initialize context to empty dict

--- palo_sidekick/main.py
+++ palo_sidekick/main.py
@@ -11,10 +11,11 @@ from palo_sidekick.helpers import get_device_groups, get_panorama
 @click.pass_context
 def cli(ctx: click.core.Context) -> None:
     """Main CLI group."""
+    ctx.ensure_object(dict)
     ctx.obj["panorama"] = get_panorama()
 
 
-@click.group()
+@cli.group()
 @click.pass_context
 def list() -> None:
     """Print lists of objects to the terminal."""
@@ -23,7 +24,8 @@ def list() -> None:
 @list.command(name="device-groups")
 @click.pass_context
 def list_device_groups(ctx: click.core.Context) -> None:
-    device_groups = get_device_groups(ctx.obj["panorama"])
+    """Print a list of device groups."""
+    device_groups = get_device_groups(ctx.panorama)
     click.echo("\n".join([dg.name for dg in device_groups]))
 
 
