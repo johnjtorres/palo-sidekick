407de5fe1461208c0608c472bb15ccbbc7680596
John Torres <johnnyjamestorres@outlook.com>
2023-03-09 16:59:25 -0800

    Add test for list device-groups command

--- tests/helpers_test.py
+++ tests/helpers_test.py
@@ -1,8 +1,10 @@
 """Tests for the helpers.py file."""
 
+from click.testing import CliRunner
 from panos.panorama import DeviceGroup
 
 from palo_sidekick.helpers import get_device_groups, get_panorama
+from palo_sidekick.main import cli
 
 PANORAMA = get_panorama()
 
@@ -11,3 +13,9 @@ def test_get_device_groups():
     devices = get_device_groups(PANORAMA)
     assert isinstance(devices, list)
     assert all(isinstance(d, DeviceGroup) for d in devices)
+
+
+def test_list_device_groups():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["list", "device-groups"])
+    assert result.exit_code == 0
